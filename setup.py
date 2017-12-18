de1293b5a3e0140b2c4dd5a4033f084a3f7ceb79
yassineameur <yassine.ameur@chauffeur-prive.com>
2017-12-18 17:25:26 +0100

    TECH add steup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='one_hot_encoder',
+    version='1.1',
+    packages=['one_hot_encoder'],
+    url='https://github.com/yassineameur/one_hot_encoder',
+    license='MIT',
+    author='Yassine Ameur',
+    author_email='yassine.ameur2013@gmail.com',
+    description='The aim of this library is to help ml practitioners to one hot encode so easily pandas dataframes'
+)
